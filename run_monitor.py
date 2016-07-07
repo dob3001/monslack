3ed3dcafc8d524c11279b5b0efc7abe854612b87
James Dobson <james.dobson@cyantechnology.com>
2016-07-07 15:44:38 +0100

    Missed out runner script, opps

---
+++ run_monitor.py
@@ -0,0 +1,12 @@
+from daemon import runner
+
+from monslack.monitor import MonSlack
+
+def run_monitor():
+    ms = MonSlack()
+    drun = runner.DaemonRunner(ms)
+    drun.do_action()
+
+
+if __name__ == "__main__":
+    run_monitor()
